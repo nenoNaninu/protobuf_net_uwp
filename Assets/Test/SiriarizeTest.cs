a6a8ea0c3d0bb1964bde5b6daa115e9e66ca0efa
nenoNaninu <neno@neno.dev>
2020-01-26 16:35:04 +0900

    protobuf netが幸せだった

---
+++ Assets/Test/SiriarizeTest.cs
@@ -0,0 +1,57 @@
+﻿using GrpcGreeter;
+using ProtoBuf;
+using System.IO;
+using UnityEngine;
+
+public class SiriarizeTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var person = new Person
+        {
+            Id = 12345,
+            Name = "しゃな～～～～～～～～～～",
+            Address = new Address
+            {
+                Line1 = "たまし！",
+                Line2 = "すわすわ！"
+            }
+        };
+
+        var ms = new MemoryStream(256 * 1024);
+        Serializer.Serialize(ms, person);
+        ms.Position = 0;
+
+        //var bytes = ms.ToArray();
+        //Debug.Log(bytes.Length);
+
+        var p2 = Serializer.Deserialize<Person>(ms);
+
+        Debug.Log(p2.Name);
+        Debug.Log(p2.Address.Line1);
+
+        Debug.Log("===============");
+
+
+        var r = new HelloReply();
+        r.Message = "いやっほーーーーーーーーー照葉たそーーーー！！";
+        
+        ms.Position = 0;
+        ms.SetLength(0);
+
+        Serializer.Serialize(ms, r);
+
+        Debug.Log(ms.ToArray().Length);
+
+        ms.Position = 0;
+        var r2 = Serializer.Deserialize<HelloReply>(ms);
+        Debug.Log(r2.Message);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
